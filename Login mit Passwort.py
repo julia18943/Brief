d5763048abe4e20fc92d93fb8fefb1bfca0e5ec1
julia18943 <97448795+julia18943@users.noreply.github.com>
2022-01-10 13:46:25 +0100

    Add files via upload

---
+++ Login mit Passwort.py
@@ -0,0 +1,23 @@
+Benutzername = str(input("Geben sie hier den Benutzernamen ein:  "))
+
+if Benutzername == "Julie123" :
+
+    print("Passwort eingeben")
+
+
+
+    passwort = str(input("Geben sie hier das Passwort ein: "))
+
+    if passwort == "Hallo   " :
+
+        print("Herzlich Willkommen")
+
+    else:
+
+        print("PW falsch")
+
+
+
+else:
+
+    print("User falsch")
